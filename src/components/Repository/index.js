a29fde0ee00741d9b0b5b766cc59963f4874b916
Rafael Pacheco <rafaeldmpacheco@gmail.com>
2020-03-24 19:46:35 -0300

    Styled Components

--- src/components/Repository/index.js
+++ src/components/Repository/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import {View} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {Container, Name, Description, Stats, Stat, StarCount} from './styles';
 
-// import { Container } from './styles';
+export default function Repository({data}) {
+  return (
+    <Container>
+      <Name>{data.name}</Name>
+      <Description>{data.description}</Description>
 
-export default function Repository() {
-  return <View />;
+      <Stats>
+        <Stat>
+          <Icon name="star" size={16} color="#333"></Icon>
+          <StarCount>{data.stars}</StarCount>
+        </Stat>
+        <Stat>
+          <Icon name="code-fork" size={16} color="#333"></Icon>
+          <StarCount>{data.stars}</StarCount>
+        </Stat>
+      </Stats>
+    </Container>
+  );
 }
